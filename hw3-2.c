8aafd2a92318d4b90cf7a58d3e23f106fe3fcea2
112310238 <146064544+112310238@users.noreply.github.com>
2023-10-18 10:29:15 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int s1,d1,s2,d2,s3,d3,car;
+    scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
+    car=1;
+    if(s2<d1)
+    {
+        car+=1;
+    }
+    if(s3<d2)
+     {
+         car+=1;
+     }
+    if(s3>=d1 )
+    {
+        car-=1;
+    }
+    printf("%d",car);
+    
